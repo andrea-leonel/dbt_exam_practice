b65acba4a175c095ccd2a9473b732e5541428e73
Andrea Leonel <andrea.leonel@hotmail.co.uk>
2025-11-09 18:30:48 +0000

    installed packages and added macros

--- models/intermediate/int_order_totals.sql
+++ models/intermediate/int_order_totals.sql
@@ -52,18 +52,8 @@ discount_emp as (
 
     select
     o.*,
-    case
-        when o.employee_orders >= 100 then 0.05
-        when o.employee_orders >= 50 then 0.03
-        when o.employee_orders >= 10 then 0.01
-        else 0.00
-    end as emp_disc,
-    case
-        when date_diff(current_date(), c.comp_dateadded, year)>= 5 then 0.10
-        when date_diff(current_date(), c.comp_dateadded, year)>= 2 then 0.05
-        when o.company_orders > 200 then 0.04
-        else 0.00
-    end as comp_disc
+    {{ disc_volume('o.employee_orders') }} as emp_disc,
+    {{ disc_loyalty('c.comp_dateadded', 'o.company_orders') }} as comp_disc
     from aggregations o
     left join employee_details c on o.emp_id = c.emp_id
 ),
